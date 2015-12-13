c01c579ceadc02f94524cd0966de46d5a8ea62f1
koktoh <koktoh96@gmail.com>
2015-12-13 21:13:33 +0900

    make Speech Recognizer

--- app/src/main/java/com/example/koktoh/voicerecognizertest/MainActivity.java
+++ app/src/main/java/com/example/koktoh/voicerecognizertest/MainActivity.java
@@ -1,27 +1,84 @@
 package com.example.koktoh.voicerecognizertest;
 
+import android.content.Intent;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private SpeechRecognizer sr;
 
     private Button riButton;
     private Button srButton;
 
     private TextView result;
 
+    private class listener implements RecognitionListener {
+        @Override
+        public void onBeginningOfSpeech() {
+        }
+
+        @Override
+        public void onBufferReceived(byte[] buffer) {
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+        }
+
+        @Override
+        public void onError(int error) {
+        }
+
+        @Override
+        public void onEvent(int eventType, Bundle params) {
+        }
+
+        @Override
+        public void onPartialResults(Bundle partialResults) {
+        }
+
+        @Override
+        public void onReadyForSpeech(Bundle params) {
+            Toast.makeText(getApplicationContext(), "Start Speech", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onResults(Bundle results) {
+            String resultStr = "";
+
+            ArrayList<String> resultList = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            for (int i = 0; i < resultList.size(); i++) {
+                resultStr += resultList.get(i) + "\n";
+            }
+            result.setText(resultStr);
+        }
+
+        @Override
+        public void onRmsChanged(float rmsdB) {
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        riButton = (Button)findViewById(R.id.riButton);
-        srButton = (Button)findViewById(R.id.srButton);
+        sr = SpeechRecognizer.createSpeechRecognizer(getApplicationContext());
 
-        result = (TextView)findViewById(R.id.textView);
+        riButton = (Button) findViewById(R.id.riButton);
+        srButton = (Button) findViewById(R.id.srButton);
+
+        result = (TextView) findViewById(R.id.textView);
 
         riButton.setOnClickListener(this);
         srButton.setOnClickListener(this);
@@ -29,12 +86,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        switch(v.getId()){
+        switch (v.getId()) {
             case R.id.riButton:
                 result.setText("hoge");
                 break;
             case R.id.srButton:
-                result.setText("hogehoge");
+                sr.setRecognitionListener(new listener());
+
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
+                sr.startListening(intent);
                 break;
         }
     }
